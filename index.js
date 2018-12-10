7532f81b630c7c52999dbf8fe5e55b957d7b3564
pyrahdys <42029512+pyrahdys@users.noreply.github.com>
2018-12-11 00:18:59 +0200

    Tehtävät 3.2-3.6

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ app.get('/', (req, res) => {
   res.send('<h1>Heippa maailma!</h1>')
 })
 
+app.get('/info', (req, res) => {
+    res.send(`<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p><p>${new Date()}</p>`)
+  })
+
 app.get('/persons', (req, res) => {
   res.json(persons)
 })
@@ -44,6 +48,39 @@ app.get('/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 100000000)
+}
+
+app.post('/persons', (req, res) => {
+  const body = req.body
+  const name = body.name.trim()
+
+  if (name === undefined || name === null || body.number === undefined || body.number === null) {
+    return res.status(400).json({error: 'name or number missing'})
+  }
+
+  if (persons.find(p => p.name.trim() === name)) {
+    return res.status(400).json({error: 'name must be unique'})
+  }
+  
+  const person = {
+    name: name,
+    number: body.number,
+    date: new Date(),
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 const PORT = 3001
