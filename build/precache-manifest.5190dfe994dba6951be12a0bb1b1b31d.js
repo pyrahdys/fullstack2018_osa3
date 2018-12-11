e2e55775a127b4a8838ce79c0f058524fd06373a
pyrahdys <42029512+pyrahdys@users.noreply.github.com>
2018-12-11 21:40:37 +0200

    Frontend committoitu backendin repositorioon

---
+++ build/precache-manifest.5190dfe994dba6951be12a0bb1b1b31d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4aca45f727cbfca0ca6b",
+    "url": "/static/js/main.4aca45f7.chunk.js"
+  },
+  {
+    "revision": "efb9f3153127ac881aa4",
+    "url": "/static/js/1.efb9f315.chunk.js"
+  },
+  {
+    "revision": "4aca45f727cbfca0ca6b",
+    "url": "/static/css/main.80421716.chunk.css"
+  },
+  {
+    "revision": "c3302008f7892a595b8787bef9a9ad8d",
+    "url": "/index.html"
+  }
+];
