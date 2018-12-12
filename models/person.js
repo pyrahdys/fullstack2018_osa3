69dfe6c2299372db6b4b740e876c6f2c66814b2e
pyrahdys <42029512+pyrahdys@users.noreply.github.com>
2018-12-13 00:55:49 +0200

    todellakin viimeinen heroku-pushaus?

--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // DELETOI SALASANA ENNE PUSHAUSTA
-const url = MONGODB_URI
+const url = process.env.MONGODB_URI
 //const url = 'mongodb://testuser:---@ds131784.mlab.com:31784/fullstack-persons'
 
 mongoose.connect(url)
